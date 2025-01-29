5e89116a43ae1f91e4885f1458631891582999d9
Ozorub <ruben.knafo@gmail.com>
2025-01-29 10:52:24 +0100

    Visualisation du jeu possible

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("Backgammon du tonnerre");
+        BgPane mPane = new BgPane();
+        Scene scene = new Scene(mPane);
+
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+
+
+    }
+
     public static void main(String[] args){
+        launch(args);
         System.out.println("Hello world");
     }
 }
